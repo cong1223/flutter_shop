98260882b61dcd26b9de51b97285fecaa55b9e38
小聪忙 <1215524451@qq.com>
2019-12-30 09:27:11 +0800

    购物车ui完成

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/model/cart_info_model.dart';
+import 'package:flutter_shop/pages/components/cart_page/cart_item.dart';
 import 'package:flutter_shop/provide/cart.dart';
 import 'package:provide/provide.dart';
+
+import 'components/cart_page/cart_bottom.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<CartInfoMode> cartList = [];
     return Scaffold(
-      appBar: AppBar(
-        title: Text('购物车'),
-      ),
-      body: Provide<CartProvider>(
-        builder: (context,child,data) {
-          return ListView.builder(
-            itemCount: data.cartList.length,
-            itemBuilder: (context,index) {
-              return ListTile(
-                title: Text(data.cartList[index].goodsName),
-                subtitle: Text(data.cartList[index].count.toString()),
-              );
-            },
-          );
-        },
-      )
-    );
+        appBar: AppBar(
+          title: Text('购物车'),
+        ),
+        body: Provide<CartProvider>(
+          builder: (context, child, data) {
+            return Stack(
+              children: <Widget>[
+                ListView.builder(
+                  itemCount: data.cartList.length,
+                  itemBuilder: (context, index) {
+                    return CartItem(data.cartList[index]);
+                  },
+                ),
+                Positioned(
+                  child: CartBottom(),
+                  bottom: 0,
+                  left: 0,
+                )
+              ],
+            );
+          },
+        ));
   }
-
 
 //  Future<String> getCartList(BuildContext context) async{
 //    await Provide.value<CartProvider>(context).getCartList();
